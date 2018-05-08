89716a16545c5d73ecb3ec1846551a004a4d30db
Mat Kolodziej <mat.kolodziej@hotmail.com>
2018-05-08 10:53:03 -0600

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
+var { mongoose } = require('./db/mongoose');
 
-var {mongoose} = require('./db/mongoose');
-
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    var todo  = new Todo({
+    var todo = new Todo({
         text: req.body.text
     });
     todo.save().then((doc) => {
@@ -22,17 +22,33 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos})
+        res.send({ todos })
     }, (e) => {
         res.status(400).send(e);
     });
 });
 
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
 
-// GET /todos
+    //Valid id using is valid
+    if (!ObjectID.isValid(id)) {
+        res.status(400);
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo})        
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
 })
 
-module.exports = {app};
+module.exports = { app };
