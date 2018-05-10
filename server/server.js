f435f47b2b286ada2406f7f3285f02f3bc296be8
Mat Kolodziej <mat.kolodziej@hotmail.com>
2018-05-10 08:01:44 -0600

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -107,10 +107,20 @@ app.post('/users', (req, res) => {
     }).catch((e) => res.status(400).send(e) );
 });
 
-
-
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password'])
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
